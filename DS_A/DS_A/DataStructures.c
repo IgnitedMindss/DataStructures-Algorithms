04d0ad661cf58df077295f02889352937a846f6e
IgnitedMindss <shubhrojyotikabiraj@gmail.com>
2020-10-17 02:01:44 +0530

    Till graph

--- DS_A/DS_A/DataStructures.c
+++ DS_A/DS_A/DataStructures.c
@@ -362,7 +362,69 @@ void print_tree(tree_node* root) {
 	printf("%d\t", root->value);
 	print_tree(root->right_child);
 }
+//===================================================================================
 
+//==============	GRAPHS		=====================================================
+
+struct edge_node {
+	int v;
+	int weight;
+	struct edge_node* next;
+};
+typedef struct edge_node edge_node;
+
+struct graph {
+	edge_node** edges;
+	int nb_nodes;
+	int nb_edges;
+	bool is_directed;
+};
+typedef struct graph graph;
+
+graph* create_graph(int nb_nodes, bool is_directed) {
+	graph* g = (graph*)(malloc(sizeof(graph)));
+	g->nb_nodes = nb_nodes;
+	g->nb_edges = 0;
+	g->is_directed = is_directed;
+	g->edges = (graph**)(malloc(sizeof(graph*) * nb_nodes));
+
+	int u;
+	for (u = 0; u < g->nb_nodes; u++)
+		g->edges[u] = NULL;
+	return g;
+}
+
+void print_graph(graph* g) {
+	int u;
+	for (u = 0; u < g->nb_nodes; u++) {
+		printf("%d-->", u);
+		
+		edge_node* e = g->edges[u];
+		while (e != NULL) {
+			printf("%d\t", e->v);
+			e = e->next;
+		}
+		printf("\n");
+	}
+}
+
+//function that will connect u to v with the weight weight
+void graph_add_edge(graph* g, int u, int v, int weight) {
+	edge_node* e = (edge_node*)(malloc(sizeof(edge_node)));
+	e->v = v;
+	e->weight = weight;
+	e->next = g->edges[u];
+	g->edges[u] = e;
+	if (!g->is_directed)		//In non-directed graph it should connect both the ways u<->v
+	{
+		e = (edge_node*)(malloc(sizeof(edge_node)));
+		e->v = u;
+		e->weight = weight;
+		e->next = g->edges[v];
+		g->edges[v] = e;
+	}
+	g->nb_edges++;
+}
 int main() {
 	//==========	FOR STACK	=====================================================
 	
@@ -463,5 +525,13 @@ int main() {
 	insert_tree(t, 5);
 	insert_tree(t, 4);
 	print_tree(t->root);
+
+	//=================================================================================
+	printf("\n");
+	//==================	FOR GRAPH		===========================================
+	graph* g = create_graph(6, false);
+	graph_add_edge(g, 2, 4, 1);
+	graph_add_edge(g, 2, 5, 1);
+	print_graph(g);
 	return 0;
 }
